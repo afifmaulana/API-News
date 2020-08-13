5c1626ef3e5b955b34757ad4d7ea6fb065ee935c
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-08-13 07:30:33 +0700

    API Category&Article

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -24,7 +24,7 @@ class ArticleController extends Controller
         return response()->json([
             'message' => 'Berhasil Menampilkan Artikel',
             'status' => true,
-            'article' => ArticleResource::collection($articles),
+            'data' => ArticleResource::collection($articles),
         ]);
     }
 
